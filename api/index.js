118483308e2562511786568b8fed6e5e792ee0df
crazydeveloper914 <crazydeveloper914@gmail.com>
2023-10-14 17:38:47 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
